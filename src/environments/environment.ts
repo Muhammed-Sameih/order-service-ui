1a956305a73516c8a2dcf5130c48c52227a469ec
The Mos <77055492+Muhammed-Sameih@users.noreply.github.com>
2023-08-28 23:27:02 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
    production: false,
    productsApi:"https://product-catalog-api-service.onrender.com/",
    storeApi:"https://store-service-api.onrender.com/",
-   orderApi:"http://localhost:8080/"
+   orderApi:"https://order-service-yn10.onrender.com/"
  };
